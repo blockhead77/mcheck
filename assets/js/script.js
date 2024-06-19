71112315b1d28b1590643ff0066871196412b9ed
blockhead77 <stuartkellock@gmail.com>
2024-06-19 15:24:30 +0100

    changed question display method, basic testing complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,18 @@
-
-// List of questions from section 1 to 5 of the m-check
-const questions = [
-{
-question: ["Rear Tyre Pressure", "Squeeze your tyre to check the air. How would you say it feels? Can you sit on the saddle without the tyre squashing too much?"],
-answer: ["Nice and firm", "A bit soft", "Totally flat"],
-rating: ["Good", "Warning", "Danger"]
-},
-{
-question: ["Tyre Condition", " Check for any cracks or splits. Look to see if the tyre tread is warn down or damaged. How would you rate it?"],
-answer: ["Great, plenty of tread", "Some cracks, bit worn", "Lots of cracks and splits, very little tread left."],
-rating: ["Good", "Warning", "Danger"]
-  },
-// More questions to be added here...
-];
+//Variables
 let currentQuestion = 0;
+let previousQuestion = -1;
 let questionsAnswered = 0;
-
-function showQuestion() {
-let questionTitle = document.getElementById("question-title");
-questionTitle.textContent = questions[currentQuestion].question[0];
-
-let questionText = document.getElementById("question-text");
-questionText.textContent = questions[currentQuestion].question[1];
-
-let answers = document.querySelector('input[value=value]').value
-  choices.forEach((choice, index) => {
-    choice.textContent = questions[currentQuestion].choices[index];
-  });
-}
+const ids = ["q0","q1","q2","q3"]
 
 
-function nextQuestion() {
+function nextQ() {
   currentQuestion++;
+  previousQuestion++;
   console.log(currentQuestion)
   questionsAnswered++;
-
-  if (currentQuestion < questions.length) {
-    showQuestion();
-  } else {
-    const quizContainer = document.querySelector(".mcheck-container");
-    quizContainer.innerHTML = `<p>You answered ${questionsAnswers}.</p>`;
+  let nextShow = document.getElementById(ids[currentQuestion]);
+  nextShow.style.display = "block";
+  let preHide = document.getElementById(ids[previousQuestion]);
+  preHide.style.display = "none"; 
   }
-}
-showQuestion();
+
