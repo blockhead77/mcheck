2d6e621674c3c37d11eacbe80b66f1ad0f88d8b2
blockhead77 <stuartkellock@gmail.com>
2024-06-24 12:32:29 +0100

    Tested question and report function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let previousQuestion = -1;
 let questionsAnswered = 0;
 const ids = ["q0","q1","q2","q3"]
 
-
+//Function to start quiz and show next question in the survery
 function nextQ() {
   currentQuestion++;
   previousQuestion++;
@@ -15,4 +15,23 @@ function nextQ() {
   let preHide = document.getElementById(ids[previousQuestion]);
   preHide.style.display = "none"; 
   }
+
+// Function to capture radio input and add it to final report
+function addtoreport() {
+  document.getElementById("report").innerHTML = "";
+  var ele = document.getElementsByTagName('input');
+  for (i = 0; i < ele.length; i++) {
+      if (ele[i].type = "radio") {
+
+          if (ele[i].checked)
+              document.getElementById("report").innerHTML
+                  += ele[i].name + ele[i].value + "<br>";
+      }
+  }
+}
+
+  //function myFunction(num) {
+  //  document.getElementById('mcheck').src = num + "bicycle_m.png";
+  //}
+  //  myFunction(1);
 
