1e372594cfca8f2d427c2ceda61bc152922358e6
blockhead77 <stuartkellock@gmail.com>
2024-06-26 21:53:41 +0100

    further layout tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //M-Check Variables
 let currentQuestion = 0;
 let previousQuestion = -1;
-const ids = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20"]
+const ids = ["q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12",]
 
 //Function to start quiz and show next question in the survery
 function nextQ() {
@@ -21,15 +21,16 @@ function showReport() {
 // Function to capture radio input and add it to final report
 function addToReport() {
   document.getElementById("report").innerHTML = " ";
-  var ele = document.getElementsByTagName('input');
+  let ele = document.getElementsByTagName('input');
   for (i = 0; i < ele.length; i++) {
     if (ele[i].type = "radio") {
 
-      if (ele[i].checked)
+      if (ele[i].checked){
         document.getElementById("report").innerHTML
           += ele[i].name + ele[i].value + "<br>";
     }
   }
+}
 }
 
 //Function to show the report
